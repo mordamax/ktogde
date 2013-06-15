051ff58c55141d995eda3d3b9c43461eec4cb379
Maksym Hlukhovtsov <mordamax@gmail.com>
2013-06-15 18:00:05 +0300

    init

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/ktogde");
+
+var placeSchema = mongoose.Schema({
+    title: 'string',
+    location: {
+        name: 'string',
+        latitude: 'number',
+        longitude: 'number'
+    }
+})
+var Place = mongoose.model("Place", placeSchema);
+
+exports.getAllPlaces = function(req, res){
+    Place.find({}, function(e, places){
+        res.json({
+            payload: places
+        })
+    })
+}
+
+exports.getOnePlace = function(req, res){
+    Place.find({_id: req.params.id}, function(e, place){
+        res.json({
+           payload: place
+        })
+    })
+}
